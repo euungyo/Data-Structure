19a90847bb209be3d0524de7b8f44931fec9e5f5
Lee Eun Gyo <ialice119@naver.com>
2025-09-29 22:19:36 +0900

    0929

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -217,10 +217,71 @@ class Main {
 
 
 
-fun main(){
-    var f1=Flower.bud.bloom("Rose")
-    var f2=Flower.bud.bloom("Hibiscus")
+//fun main(){
+//    var f1=Flower.bud.bloom("Rose")
+//    var f2=Flower.bud.bloom("Hibiscus")
+//
+//    println(f1)
+//    println(f2)
+//}
 
-    println(f1)
-    println(f2)
+
+
+
+
+
+//fun makeBeverageTree(): TreeNode<String> {
+//    val tree = TreeNode("Beverages")
+//    val hot = TreeNode("hot")
+//    val cold = TreeNode("cold")
+//    val tea = TreeNode("tea")
+//    val coffee = TreeNode("coffee")
+//    val chocolate = TreeNode("cocoa")
+//    val blackTea = TreeNode("black")
+//    val greenTea = TreeNode("green")
+//    val chaiTea = TreeNode("chai")
+//    val soda = TreeNode("soda")
+//    val milk = TreeNode("milk")
+//    val gingerAle = TreeNode("ginger ale")
+//    val bitterLemon = TreeNode("bitter lemon")
+//    tree.add(hot)
+//    tree.add(cold)
+//    hot.add(tea)
+//    hot.add(coffee)
+//    hot.add(chocolate)
+//    cold.add(soda)
+//    cold.add(milk)
+//    tea.add(blackTea)
+//    tea.add(greenTea)
+//    tea.add(chaiTea)
+//    soda.add(gingerAle)
+//    soda.add(bitterLemon)
+//    return tree
+//}
+//
+//fun main(){
+//    val tree = makeBeverageTree()
+//    tree.forEachDepthFirst { println(it.value) }
+//}
+
+
+
+
+fun makeBinaryTree(): BinaryNode<Int> {
+    val zero = BinaryNode(0)
+    val one = BinaryNode(1)
+    val five = BinaryNode(5)
+    val seven = BinaryNode(7)
+    val eight = BinaryNode(8)
+    val nine = BinaryNode(9)
+    seven.leftChild = one
+    one.leftChild = zero
+    one.rightChild = five
+    seven.rightChild = nine
+    nine.leftChild = eight
+    val tree = seven
+    return tree
+}
+fun main(){
+    val tree = makeBinaryTree()
 }
--- src/main/kotlin/QueueInterface.kt
+++
@@ -1,9 +0,0 @@
-interface QueueInterface<T> {
-    fun enqueue(value: T): Boolean
-    fun dequeue(): T?
-    val count: Int
-        get
-    val isEmpty: Boolean
-            get() = count == 0
-    fun peek(): T?
-}
