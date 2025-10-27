0254af95596c733b09fa1ec25d62367fab49886c
Lee Eun Gyo <ialice119@naver.com>
2025-10-27 23:42:07 +0900

    1027

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -33,15 +33,15 @@ class Main {
 
 
 
-//
-//fun main(){
-//    var o1: ChildOfInterface = ChildOfInterface(10)
+
+fun main(){
+    var o1: ChildOfInterface = ChildOfInterface(10)
 //    println(o1)
 //    println(o1.f1())
-//    println(o1.f2("A"))
+    println(o1.f2("A"))
 //    println(o1.f3("B"))
 //    println(o1.f4())
-//}
+}
 
 
 
@@ -288,25 +288,25 @@ class Main {
 
 
 
-fun makeAVLTree(): AVLNode<Int>{
-    val one = AVLNode(1)
-    val seven = AVLNode(7)
-    val eight = AVLNode(8)
-    val nine = AVLNode(9)
-    val eleven = AVLNode(11)
-    val ten = AVLNode(10)
-    val fifteen =  AVLNode(15)
-    seven.leftChild = one
-    seven.rightChild = nine
-    nine.leftChild = eight
-    nine.rightChild = eleven
-    eleven.leftChild = ten
-    eleven.rightChild = fifteen
-    return seven
-}
-
-fun main(){
-    var tree = makeAVLTree()
-    println(tree)
-    println("height: ${tree.height()}")
-}
+//fun makeAVLTree(): AVLNode<Int>{
+//    val one = AVLNode(1)
+//    val seven = AVLNode(7)
+//    val eight = AVLNode(8)
+//    val nine = AVLNode(9)
+//    val eleven = AVLNode(11)
+//    val ten = AVLNode(10)
+//    val fifteen =  AVLNode(15)
+//    seven.leftChild = one
+//    seven.rightChild = nine
+//    nine.leftChild = eight
+//    nine.rightChild = eleven
+//    eleven.leftChild = ten
+//    eleven.rightChild = fifteen
+//    return seven
+//}
+//
+//fun main(){
+//    var tree = makeAVLTree()
+//    println(tree)
+//    println("height: ${tree.height()}")
+//}
